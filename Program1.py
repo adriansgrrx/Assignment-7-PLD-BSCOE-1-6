164114cdb20a8fe694a9d44569e28b793a6f5c41
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-11-27 23:46:35 +0800

    PROGRAM 1 UNCOMPLETE: Just finished testing the vowel and consonant assignment and it went well. I also added time functionality and im planning to add another variable

--- Program1.py
+++ Program1.py
@@ -25,3 +25,29 @@ def get_calculation(): # def functionality
     # List of the assignments stored in a variable
     consonants = ["b","B","c","C","d","D","f","F","g","G","h","H","j","J","k","K","l","L","m","M","n","N","p","P","q","Q","r","R","s","S","t","T","v","V","w","W","x","X","y","Y","z","Z"]
     vowels = ["a","A","e","E","i","I","o","O","u","U"]
+    """
+    Repetition and if-else statement section
+    """
+    for char in yourSentence:
+        if char in vowels:
+            vowelC = vowelC + 1
+        if char in consonants:
+            consonantC = consonantC + 1
+    return vowelC, consonantC
+
+#Calling out the function
+vowelC, consonantC = get_calculation()
+
+
+# 1sec delay just for beautification 
+print("\n[PROCESSING]\nPlease wait for three(3) seconds...\n")
+time.sleep(1)
+print("[3]")
+time.sleep(1)
+print("[2]")
+time.sleep(1)
+print("[1]")
+time.sleep(1)
+# Display the result of the text evaluation
+print("\nThe system successfully scanned your text!")
+print(f"\nWord count: \nVowel count: {vowelC}\nConsonant count: {consonantC}\n\n[Additional Text Information]\nCharacter count: \nSpace count:\n")
