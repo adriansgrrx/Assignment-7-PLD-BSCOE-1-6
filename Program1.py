50f21a17532631e5605794168d67a3fda5dfa6c7
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-11-28 00:14:28 +0800

    PROGRAM 1 NEARLY TO BE COMPLETED: I add another global variable , I also modified some print statements and I just tested it and it work fine.

--- Program1.py
+++ Program1.py
@@ -16,7 +16,8 @@ import time
 yourSentence = input("\nType or paste the text you wish to be analyze:\n")
 def get_calculation(): # def functionality
     # essential global variables
-    wordC = 0   # wordC = Word count
+    sentenceL = len(yourSentence) # storing length functionality to the variable sentenceL =  Sentence length
+    wordC = 1   # wordC = Word count
     vowelC = 0      # vowelC = Vowel count
     consonantC = 0      # consonantC = Consonant count
     # additional secondary text information
@@ -28,16 +29,21 @@ def get_calculation(): # def functionality
     """
     Repetition and if-else statement section
     """
-    for char in yourSentence:
-        if char in vowels:
-            vowelC = vowelC + 1
-        if char in consonants:
-            consonantC = consonantC + 1
-    return vowelC, consonantC
-
+    for char_slate in range (0, sentenceL):
+        char = yourSentence[char_slate]
+        if char != " ":
+                charC = charC + 1
+                if char in vowels:
+                    vowelC = vowelC + 1
+                if char in consonants:
+                    consonantC = consonantC + 1    
+        else:
+            spaceC = spaceC + 1
+            if char_slate < sentenceL:
+                wordC = wordC + 1
+    return vowelC, consonantC, wordC, charC, spaceC, sentenceL
 #Calling out the function
-vowelC, consonantC = get_calculation()
-
+vowelC, consonantC, wordC, charC, spaceC, sentenceL = get_calculation()
 
 # 1sec delay just for beautification 
 print("\n[PROCESSING]\nPlease wait for three(3) seconds...\n")
@@ -49,5 +55,5 @@ time.sleep(1)
 print("[1]")
 time.sleep(1)
 # Display the result of the text evaluation
-print("\nThe system successfully scanned your text!")
-print(f"\nWord count: \nVowel count: {vowelC}\nConsonant count: {consonantC}\n\n[Additional Text Information]\nCharacter count: \nSpace count:\n")
+print(f"\nThe system successfully scanned your text!\nYour text: {yourSentence}")
+print(f"\nWord count: {wordC}\nVowel count: {vowelC}\nConsonant count: {consonantC}\n\n[Additional Text Information]\nCharacter count: {charC}\nSpace count:{spaceC}\n")
