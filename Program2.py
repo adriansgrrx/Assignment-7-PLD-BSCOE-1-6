d3ffe6d71f262281a2abc60a1f703e1f55ce8370
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-11-28 12:35:17 +0800

    PROGRAM 2 UNCOMPLETE: Complete all the task needed and its working. Needed to add loop and add some re commands if needed.

--- Program2.py
+++ Program2.py
@@ -14,8 +14,24 @@ Regular Expression (RegEx) module will be used in this program
 """
 import re   # regex module
 
+# Ask for the user input
 password = input("enter ur pass: ")
-if (len(password) > 15):
-    print("Ok good")
-else:  
-    print("bad")
+
+# f-elif statements section
+if (len(password) < 15)is None: # len functionality to determine the length of the input then compete with the integer 15(standard char input)
+    print("passsword must be 15 characters and above") # if satisfied, display.
+
+    """
+from regular exp. module, .search(pattern, string) command is used to scan/search character(s) inside the string/list
+    """
+# I used [] to indicate list.
+elif re.search(r"[A-Z]", password)is None:  # "r" is used to modify "raw-string" and 'is None' is provided if it does not contain the charcters
+    print("password must contain Capital letters")
+elif re.search(r"[a-z]", password)is None: # " "
+    print("password must contain Small letters") # " "
+elif re.search(r"\d", password)is None: # '\d' special char to indicate digits/ numbers 0-9
+    print("password mus have D")
+elif re.search(r"[!@#$%&]", password)is None:
+    print("password must have SChar")
+elif re.search(r"[a-z A-Z 0-9 !@#$%&]", password): # sum-p all the input to proceed on valid password
+    print("Valid password!")
